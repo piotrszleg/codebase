8c5a8ef69a137b0d5bd9dad0b21654dad478f7ae
Piotr Szlęg <szlegp@gmail.com>
2018-05-20 03:49:26 +0200

    posts.js use mongo and promises

--- posts.js
+++ posts.js
@@ -1,79 +1,89 @@
-/*
-Saving posts to mysql database and retrieving them.
-*/
-
-var mysql = require('mysql');
-
-//mysql
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "accounts"
-});
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected to the mysql database.");
-});
-
-// post : {owner, title, text}
-exports.create=(post, callback)=>{
-	var sql = "INSERT INTO posts (owner, title, text) VALUES ?";
-	con.query(sql, [[[post.owner, post.title, post.text]]], function (err, result) {
-        if (err) {
-            console.error(err);
-            callback(null);
-       } else {
-           callback(result);
-       }
-    });
+var MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/codebase";
+// get the database object
+var db=null;
+MongoClient.connect(url)
+	.then(function callback(client){db=client.db("codebase")})
+  .catch((err)=>{throw err});
+  
+// returns a Promise that resolves when condition evaluates to non falsy value
+function promiseWhen(condition, timeout){
+	if(!timeout){
+		timeout = 2000;
+	}
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>reject(new Error("Promise when expired.")), timeout);// reject promise if given timeout is exceeded
+		function loop(){
+			if(condition()){//check condition
+				resolve();
+			} else {
+				setTimeout(loop,0);
+			}
+		}
+		setTimeout(loop,0);
+	});
 }
 
-// updated : {title, text, id}
-exports.edit=(id, updated, callback)=>{
-  var sql = "UPDATE posts SET title=?, text=? WHERE id=?";
-	con.query(sql, [updated.title, updated.text, id], function (err, result) {
-        if (err) {
-            console.error(err);
-            callback(null);
-       } else {
-           callback(result);
-       }
-    });
-}
-
-exports.delete=(id, callback)=>{
-  var sql = "DELETE FROM posts WHERE id=?";
-	con.query(sql, [id], function (err, result) {
-        if (err) {
-            console.error(err);
-            callback(null);
-       } else {
-           callback(result);
-       }
-    });
-}
-
-exports.get=(id, callback)=>{
-    var sql = "SELECT * FROM posts WHERE id=?";
-    con.query(sql, id, function (err, result) {
-		if (err) {
-			 console.error(err);
-			 callback(null);
-		} else {
-			callback(result[0]);
+exports.create=(author, title, content)=>{
+	return new Promise(async (resolve, reject)=>{
+		try {
+      await promiseWhen(()=>db);
+        var id = (await db.collection("posts").insertOne(
+                {"author" : author, "title" : title, "content" : content})).insertedId;
+				resolve(id);
+		} catch(err){
+			reject(err);
 		}
 	});
 }
 
-exports.getAll=(callback)=>{
-  var sql = "SELECT * FROM posts";
-  con.query(sql, function (err, result) {
-  if (err) {
-     console.error(err);
-     callback(null);
-  } else {
-    callback(result);
-  }
-});
+exports.edit=(id, author, title, content)=>{
+  return new Promise(async (resolve, reject)=>{
+		try {
+			await promiseWhen(()=>db);
+				resolve(await db.collection("posts").updateOne(
+          {"_id":id},
+					{$set : {"author" : author, "title" : title, "content" : content}}
+				));
+		} catch(err){
+			reject(err);
+		}
+	});
+}
+
+exports.remove=(id)=>{
+	return new Promise(async (resolve, reject)=>{
+		try {
+			await promiseWhen(()=>db);
+			await db.collection("posts").remove({"_id" : id});
+			resolve();
+		} catch(err){
+			reject(err);
+		}
+	});
+}
+
+exports.get=(id)=>{
+	return new Promise(async (resolve, reject)=>{
+		try {
+			await promiseWhen(()=>db);
+			resolve((await db.collection("posts").findOne(
+				{"_id" : id}
+			)));
+		} catch(err){
+			reject(err);
+		}
+	});
+}
+
+exports.all=()=>{
+  return new Promise((resolve, reject)=>{
+		promiseWhen(()=>db)
+		.then(()=>{
+			db.collection("posts").find({}).toArray()
+				.then(resolve)
+				.catch(reject);
+		})
+		.catch(reject);
+  });
 }
--- tests.js
+++ tests.js
@@ -1,6 +1,5 @@
-var accounts = require("./accounts");
-
-(async function(){
+async function testAccounts(){
+    var accounts = require("./accounts");
     try{
         const test_user={"username" : "test_user", "password" : "test_password"}
         await accounts.remove(test_user.username);
@@ -17,4 +16,22 @@ var accounts = require("./accounts");
         console.log(err);
     }
     process.exit();
-})();
+}
+
+async function testPosts(){
+    var posts = require("./posts");
+    try{
+        const test_post={"author" : "test_author", "title" : "test_title", "content" : "test_content"}
+        var id=await posts.create(test_post.author, test_post.title, test_post.content);
+        console.log(await posts.get(id));
+        console.log(await posts.all());
+        await posts.edit(id, test_post.author, test_post.title+" edited", test_post.content);
+        console.log(await posts.get(id));
+        await posts.remove(id);
+        console.log("removed the post.");
+    }catch(err){
+        console.log(err);
+    }
+    process.exit();
+}
+testPosts();
